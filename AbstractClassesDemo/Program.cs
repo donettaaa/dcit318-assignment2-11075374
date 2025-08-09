2455def58ff9386203cc4181025e41210f3c7ef0
donettaaa <dsdodoh@st.ug.edu.gh>
2025-08-09 01:05:56 +0000

    Add AbstractClassesDemo project for Task 2

---
+++ AbstractClassesDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle c = new Circle(5);
+        Rectangle r = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle area: {c.GetArea()}");
+        Console.WriteLine($"Rectangle area: {r.GetArea()}");
+    }
+}
+
